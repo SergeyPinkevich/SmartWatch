953c8bf4d9f3fb3acdf3a48451002d43e5f7cbcd
Sergey Pinkevich <sergeipinkevich163@gmail.com>
2021-01-22 19:41:27 +0200

    setup for wear module

---
+++ wear/src/main/java/com/mockingbird/spinkevich/wear/WearActivity.kt
@@ -0,0 +1,12 @@
+package com.mockingbird.spinkevich.wear
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+
+class WearActivity : FragmentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wear)
+    }
+}
